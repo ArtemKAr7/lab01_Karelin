42e9ddb642f0aa3281773756b6b0990aafe11600
ArtemKAr7 <144451988+ArtemKAr7@users.noreply.github.com>
2023-09-08 18:00:40 +0300

    Add files via upload

---
+++ Lab 01 Karelin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01_Karelin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            ///////////////////////////////////////
+            // учебная часть 
+            /////////////////////////////
+            
+              sbyte a = -15;
+              sbyte c = 100;
+              sbyte b=Math.Abs(a);
+            // Console.WriteLine(b);
+            sbyte d = Math.Max( a, c);            
+             // Console.WriteLine(d);
+
+            
+                        double R = 46.8;
+                        double e = Math.Sqrt(R);
+                       // Console.WriteLine(e);
+
+            ////////////////////////////////////////////////////
+            //Индивидуальное задание
+            ////////////////////////////////
+            /* 8. Вычислить площадь круга радиуса R. Максимально задействовать
+* имеющиеся в распоряжении функции модуля «Математика».
+    */
+            double r = 5;
+            double area = Math.PI * Math.Pow(r,2);
+            Console.WriteLine("Площадь круга =" + area);
+            // ответ 78,5398163...
+
+        }
+    }
+}
